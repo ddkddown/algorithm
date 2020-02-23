8eff4419c248c7023dbc287df574b3031e4b6d0c
ddkdown <2628397115@qq.com>
2020-02-23 16:29:13 +0800

    增加简单快速排序

--- ddk_algorithm/sort.hpp
+++ ddk_algorithm/sort.hpp
@@ -91,63 +91,109 @@ void shell_sort(T* arr = nullptr, len_size len = 0, bool order = false){
  *@return: true 排序成功 false 排序失败
  *@complex_rate:O(nlogn)
  */
-template<typename T = int>
-void merge(T* arr = nullptr, len_size lo = 0, len_size mid = 0,
-                  len_size hi = 0, T* arr_tmp = nullptr ,bool order = false){
-    len_size i = lo, j = mid+1;
+namespace merge_sort{
+    template<typename T = int>
+    void merge(T* arr = nullptr, len_size lo = 0, len_size mid = 0,
+                      len_size hi = 0, T* arr_tmp = nullptr ,bool order = false){
+        len_size i = lo, j = mid+1;
     
     
-    for(len_size k = lo; k <= hi; ++k){
-        arr_tmp[k] = arr[k];
+        for(len_size k = lo; k <= hi; ++k){
+            arr_tmp[k] = arr[k];
+        }
+    
+        for(len_size k = lo; k <= hi; ++k){
+            if(i > mid){
+                arr[k] = arr_tmp[j++];
+            }
+            else if(j > hi){
+                arr[k] = arr_tmp[i++];
+            }
+            else if((arr_tmp[i] < arr_tmp[j] && order) ||
+                    (arr_tmp[i] > arr_tmp[j] && !order)){
+                arr[k] = arr_tmp[i++];
+            }
+            else if ((arr_tmp[i] > arr_tmp[j] && order) ||
+                     (arr_tmp[i] < arr_tmp[j] && !order)){
+                arr[k] = arr_tmp[j++];
+            }
+        }
     }
+
+    template<typename T = int>
+    void sort(T* arr = nullptr, len_size lo = 0, len_size hi = 0,
+                        T* arr_tmp = nullptr,bool order = false){
     
-    for(len_size k = lo; k <= hi; ++k){
-        if(i > mid){
-            arr[k] = arr_tmp[j++];
+        if(hi <= lo){
+            return;
         }
-        else if(j > hi){
-            arr[k] = arr_tmp[i++];
+        if((hi - lo) <= 6){
+            insertion_sort(arr,hi-lo+1,order);
         }
-        else if((arr_tmp[i] < arr_tmp[j] && order) ||
-                (arr_tmp[i] > arr_tmp[j] && !order)){
-            arr[k] = arr_tmp[i++];
+    
+        len_size mid = lo + (hi - lo)/2;
+        sort(arr, lo, mid, arr_tmp, order);
+        sort(arr, mid+1, hi, arr_tmp, order);
+        merge(arr, lo, mid, hi, arr_tmp, order);
+        for(len_size i = lo; i<=hi; ++i){
+            std::cout<<arr[i];
         }
-        else if ((arr_tmp[i] > arr_tmp[j] && order) ||
-                 (arr_tmp[i] < arr_tmp[j] && !order)){
-            arr[k] = arr_tmp[j++];
+        std::cout<<std::endl;
+    
+    }
+
+    template<typename T = int>
+    void merge_sort(T* arr = nullptr, len_size len = 0,bool order = false){
+        try {
+            T* arr_tmp = new T[len];
+            sort(arr,0,len-1,arr_tmp,order);
+        }catch(std::exception e){
+            return;
         }
     }
 }
+/**
+ * @breif 简单快速排序
+ */
+namespace quick_sort{
+    template<typename T = int>
+    bool less(T& a, T& b){
+        return a<b?true:false;
+    }
 
-template<typename T = int>
-void sort(T* arr = nullptr, len_size lo = 0, len_size hi = 0,
-                    T* arr_tmp = nullptr,bool order = false){
-    
-    if(hi <= lo){
-        return;
-    }
-    if((hi - lo) <= 6){
-        insertion_sort(arr,hi-lo+1,order);
-    }
-    
-    len_size mid = lo + (hi - lo)/2;
-    sort(arr, lo, mid, arr_tmp, order);
-    sort(arr, mid+1, hi, arr_tmp, order);
-    merge(arr, lo, mid, hi, arr_tmp, order);
-    for(len_size i = lo; i<=hi; ++i){
-        std::cout<<arr[i];
-    }
-    std::cout<<std::endl;
-    
-}
+    template<typename T = int>
+    int partition(T* arr = nullptr, int lo = 0, int hi = 0){
+        //数组分为[lo...i-1],[i],[i+1...hi]
+        int i = lo, j = hi+1;
+        auto tmp = arr[lo];
+        while(1){
+            while(less(arr[++i], tmp)) if(i == hi) break;
+            while(less(tmp, arr[--j])) if(j == lo) break;
+            if(i >= j){
+                break;
+            }
 
-template<typename T = int>
-void merge_sort(T* arr = nullptr, len_size len = 0,bool order = false){
-    try {
-        T* arr_tmp = new T[len];
-        sort(arr,0,len-1,arr_tmp,order);
-    }catch(std::exception e){
-        return;
+            exchange(arr[i],arr[j]);
+        }
+
+        exchange(arr[lo],arr[j]);
+        return j;
+    }
+
+    template<typename T = int>
+    void sort(T* arr = nullptr, int lo = 0, int hi = 0){
+        if(hi <= lo){
+            return;
+        }
+
+        int j = partition(arr,lo,hi);
+        sort(arr,lo,j-1);
+        sort(arr,j+1,hi);
+    }
+
+    template<typename T = int>
+    void quick_sort(T* arr = nullptr, len_size len = 0){
+        sort(arr,0,len-1);
     }
 }
 }
