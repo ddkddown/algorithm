0d137dd01ce99177b9d451b2aae6c0ecc2354675
ddkdown <2628397115@qq.com>
2020-05-11 22:38:43 +0800

    提交新题解

---
+++ 快速求幂/main.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:
+	double quickMul(double x, long long N){
+		if(0 == N){
+			return 1.0;
+		}
+
+		double y = quickMul(x, N/2);
+		return N % 2 == 0 ? y * y : y * y * x;
+	}
+
+	double myPow(double x, int n){
+		long long N = n;
+		return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+	}
+
+};
