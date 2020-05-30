eda0fe0034d3e18884df95cf931cef6881a8e414
ddkdown <2628397115@qq.com>
2020-05-30 14:15:48 +0800

    增加冒泡排序

--- ddk_algorithm/sort.hpp
+++ ddk_algorithm/sort.hpp
@@ -58,6 +58,30 @@ void insertion_sort(T* arr = nullptr, len_size len = 0, bool order = false){
         }
     }
 }
+/*
+ *@brief 冒泡排序
+ *@param arr 数组, len 数组长度 , order false: 降序 true: 升序
+ *@complex_rate:O(N^2)
+ */
+template<typename T = int>
+void bubble_sort(T* arr = nullptr, len_size len = 0, bool order = false){
+	if(!arr){
+		return;
+	}
+
+	for(len_size i = 0; i < len; ++i){
+		for(len_size j = 1; j < len-i; ++j){
+			if((arr[j] < arr[j-1] && order) ||
+			(arr[j] > arr[j-1] && !order)){
+				exchange(arr[j], arr[j-1]);
+			}
+		}
+
+	}
+
+}
+
+
 
 /*
  *@brief: 希尔排序
