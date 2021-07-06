c9fed818424eabada5e8f09ab46c5011635a6f5b
ddkddown <2628397115@qq.com>
2021-07-06 23:58:48 +0800

    [update] add solution

--- 2021社招复习/1.cpp
+++ 2021社招复习/1.cpp
@@ -7,6 +7,7 @@
 #include <map>
 using namespace std;
 
+//数组无序的情况
 vector<int> solution(vector<int> &a, vector<int> &b) {
     map<int, int> m;
     for(auto &i : a) {
@@ -24,7 +25,26 @@ vector<int> solution(vector<int> &a, vector<int> &b) {
     return ret;
 }
 
+//数组有序的情况
+vector<int> solution_2(vector<int> &a, vector<int> &b) {
+    int i = 0, j = 0;
+    vector<int> ret;
 
+    while(1) {
+        if(i >= a.size()) break;
+        if(j >= b.size()) break;
+
+        if(a[i] < b[j]) ++i;
+        else if(a[i] > b[j]) ++j;
+        else {
+            ret.push_back(a[i]);
+            ++i;
+            ++j;
+        }
+    }
+
+    return ret;
+}
 int main() {
     vector<int> a = {1,2,2,1}, b = {2,2};
 
@@ -33,6 +53,7 @@ int main() {
     for(auto &i : c) {
         cout<<i<<endl;
     }
+    cout<<"----------------"<<endl;
 
     a = {4,9,5};
     b = {9,4,9,8,4};
@@ -40,6 +61,13 @@ int main() {
     for(auto &i : c) {
         cout<<i<<endl;
     }
+    cout<<"----------------"<<endl;
     
+    a = {4,5,6,7,8,9};
+    b = {6,7,8,9};
+    c = solution_2(a, b);
+    for(auto &i : c) {
+        cout<<i<<endl;
+    }
 }
 
